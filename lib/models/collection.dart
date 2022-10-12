5841d558a653231e6a80163c5b4bde4c049bbec8
Jordi <55429631+jorbush@users.noreply.github.com>
2022-10-12 12:28:08 +0200

    collection and database structure

--- lib/models/collection.dart
+++ lib/models/collection.dart
@@ -3,14 +3,30 @@ import './card.dart';
 
 class Collection {
   final List<CardItem> cards;
-  final String collectionImg;
+
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+
   bool isSelected;
   bool isBought;
 
   Collection({
     @required this.cards,
-    @required this.collectionImg,
+    @required this.id,
+    @required this.title,
+    @required this.description,
+    @required this.price,
+    @required this.imageUrl,
+    this.isSelected = false,
+    this.isBought = false,
   });
+
+  List<CardItem> get cardsItems {
+    return [...cards];
+  }
 
   void setSelected() {
     isSelected = true;
--- lib/providers/collections.dart
+++ lib/providers/collections.dart
@@ -1,2 +1,33 @@
-// Desde este preveedor se gestionará tanto laas colecciones 
+// Desde este preveedor se gestionará tanto laas colecciones
 // que compre el usuario como las que seleccione para el juego
+import 'package:flutter/foundation.dart';
+import '../models/collection.dart';
+
+class Collections with ChangeNotifier {
+  List<Collection> _collections = [];
+
+  Collections();
+
+  List<Collection> get collections {
+    return [..._collections];
+  }
+
+  List<Collection> get selectedCollections {
+    // return collections which isSelected
+  }
+
+  Future<void> fetchAndSetCollections() async {
+    // TODO: Get collections data from firebase
+    // FireBase will store the collection price and if is already bought
+    // Here we need to set this parameters for each collection
+    notifyListeners();
+  }
+
+  void setCollectionSelected(String id) {
+    // TODO
+  }
+
+  void buyCollection(String id) {
+    // TODO
+  }
+}
